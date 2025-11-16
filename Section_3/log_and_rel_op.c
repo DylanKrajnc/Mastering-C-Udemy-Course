bad0df42583365cb2e4b6aa79a4ecc13c505700a
DylanKrajnc <dylan_krajnc@outlook.com>
2025-11-16 10:49:34 +0000

    Section 3 - Lecture 18

---
+++ Section_3/log_and_rel_op.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+  /*Logical operators
+  * ==, !=, && (logical and), || (inclusive or), !(not operator)
+  */
+
+  /*Relational operators
+  * <, >, <=, >=
+  */
+
+  printf("%d\n", !0);
+  
+  return 0;
+}
