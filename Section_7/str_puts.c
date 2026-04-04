b6a7d5369b447029466e477d3745db18a013fed9
DylanKrajnc <dylan_krajnc@outlook.com>
2026-04-05 00:34:10 +0100

    Section 7 - Lecture 76

---
+++ Section_7/str_puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+  printf("%s\n", "Hello!");
+  puts("Hello!"); // automatically puts newline character
+  // gets - depricated
+  // fgets - still usable (getting string from console)
+
+  return 0;
+}
